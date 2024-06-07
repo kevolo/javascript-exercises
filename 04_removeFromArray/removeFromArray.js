1ce9bda1db8a1ead30ca372719e0d8ae6f5bcb85
kevolo <klogan@optonline.net>
2024-06-07 11:57:31 -0400

    Changes to pass all test cases

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    let retArr = arr.filter(val => {
+        for (const item of items) {
+            if (val===item) {
+                return false;
+            }
+        }
+        return true; 
+    });
+    return retArr;
 };
 
 // Do not edit below this line
