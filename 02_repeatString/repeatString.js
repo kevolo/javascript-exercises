47ef8d5f5d1e973d8c1ea68191efa68bab8c3ebc
kevolo <klogan@optonline.net>
2024-06-07 11:19:18 -0400

    Changes to pass all test cases

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(text, multiplier) {
+    if (multiplier < 0) {
+        return "ERROR";
+    }
+    if (text==="") {
+        return "";
+    }
+    let retString = "";
+    for (let i=0; i<multiplier; i++) {
+        retString += text;
+    }
+    return retString;
 };
 
 // Do not edit below this line
