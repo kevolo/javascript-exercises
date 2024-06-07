d0a5c5524c82b8d08ae7d25b3293c68c6298e0d5
kevolo <klogan@optonline.net>
2024-06-07 11:32:27 -0400

    Changes to fix failing tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    if (text==="") {
+        return "";
+    }
+    let retString = "";
+    for (let i=0; i<text.length; i++) {
+        retString += text.charAt(text.length - i -1);
+    }
+    return retString;
 };
 
 // Do not edit below this line
