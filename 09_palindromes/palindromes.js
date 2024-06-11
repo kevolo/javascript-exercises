658fbbc30e3103210e303261616c365321192a7a
kevolo <klogan@optonline.net>
2024-06-11 12:35:59 -0400

    Change to make all test cases pass.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+  const cleanText = text.toLowerCase().replace(/[^a-zA-Z0-9]+/g,"");
+  const temp = [...cleanText].reverse().join("");
+  if (temp === cleanText) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
