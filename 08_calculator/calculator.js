c6bf037f05fa38a8aecc411ae872ee3027dfd326
kevolo <klogan@optonline.net>
2024-06-11 12:16:01 -0400

    Changes to pass all test cases

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc,item) => acc+item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc,item) => acc*item, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1,num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num===0) {
+    return 1;
+  }
+  let fact = 1;
+  for (let i=1; i<=num; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
